813a6dae8fa3df2c01957d4174e632645a9952da
Zach Grande <zach.grande23@gmail.com>
2026-01-03 17:15:58 -0800

    fix(test): configure simplecov for rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,13 @@
 ENV["RAILS_ENV"] ||= "test"
 
 require "simplecov"
-SimpleCov.start
+SimpleCov.start "rails" do
+  enable_coverage :branch
+  add_filter "/test/"
+  add_filter "/config/"
+  add_filter "/db/"
+  add_filter "/vendor/"
+end
 
 require_relative "../config/environment"
 require "rails/test_help"
@@ -11,6 +17,15 @@ module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
+
+    # Properly merge SimpleCov results from parallel test processes
+    parallelize_setup do |worker|
+      SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+    end
+
+    parallelize_teardown do |worker|
+      SimpleCov.result
+    end
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
